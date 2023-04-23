733e8e1c3bee24aa480a7d17ed39b1048b7f7737
Ryan B <bertramdryan@gmail.com>
2023-04-23 16:02:29 -0400

    second commit

--- starship_liftoff_simulator.rb
+++ starship_liftoff_simulator.rb
@@ -1,38 +1,46 @@
-# Set constants for the Starship rocket system
-ORBITAL_LAUNCH_MOUNT_HEIGHT = 100 # feet
-TOTAL_ENGINES = 33
-ROCKET_WEIGHT = 1410000 # pounds
-FUEL_DENSITY = 51.6 # pounds per gallon
-FUEL_EFFICIENCY = 300 # seconds of specific impulse
-PAYLOAD_WEIGHT = 10000 # pounds
-GRAVITY_ACCELERATION = 32.174 # feet per second squared
+# Constants
+ENGINE_THRUST = 1900000 # Newtons
+DRY_ROCKET_MASS = 120000 # kg
+MAX_FUEL_MASS = 1200000 # kg
+FUEL_FLOW_RATE = 0.685 # kg/second/engine
+GRAVITY = 9.81 # m/s^2
+MIN_LIFTOFF_THRUST = 1.2 # Multiplier for minimum viable lift-off thrust
 
-# Calculate the minimum viable lift-off thrust
-minimum_thrust = (ROCKET_WEIGHT + PAYLOAD_WEIGHT) * GRAVITY_ACCELERATION / TOTAL_ENGINES
+# Initial conditions
+fuel_mass = 0 # kg
+total_mass = DRY_ROCKET_MASS + fuel_mass
+acceleration = ENGINE_THRUST * MIN_LIFTOFF_THRUST / total_mass - GRAVITY
+velocity = 0
+height = 0
 
-# Simulate liftoff with incremental engine activation
-current_height = 0
-current_thrust = 0
-current_engines = 0
-fuel_gallons = 0
-while current_height < ORBITAL_LAUNCH_MOUNT_HEIGHT
-  # Increment the number of engines until minimum viable thrust is reached
-  while current_thrust < minimum_thrust
-    current_engines += 1
-    current_thrust = current_engines * minimum_thrust
+# Simulate liftoff
+while height < 30.48 # 100 feet in meters
+  if total_mass < MAX_FUEL_MASS
+    fuel_mass += FUEL_FLOW_RATE * MIN_LIFTOFF_THRUST
+    fuel_mass = [fuel_mass, MAX_FUEL_MASS - DRY_ROCKET_MASS].min
   end
   
-  # Calculate the fuel used by the engines
-  fuel_flow_rate = current_engines * 150 # assume 150 gallons per minute per engine
-  fuel_used = fuel_flow_rate / FUEL_EFFICIENCY
-  fuel_gallons += fuel_used
-  
-  # Calculate the new rocket weight after using fuel
-  current_weight = ROCKET_WEIGHT + fuel_gallons * FUEL_DENSITY + PAYLOAD_WEIGHT
-  
-  # Calculate the new height using the rocket equation
-  delta_v = current_thrust / current_weight * FUEL_EFFICIENCY
-  current_height += delta_v * FUEL_EFFICIENCY / 2 / GRAVITY_ACCELERATION
-  
-  puts "At #{current_engines} engines and #{fuel_gallons.round(2)} gallons of fuel, reached #{current_height.round(2)} feet of lift"
+  total_mass = DRY_ROCKET_MASS + fuel_mass
+  acceleration = ENGINE_THRUST * MIN_LIFTOFF_THRUST / total_mass - GRAVITY
+  velocity += acceleration * 0.1 # 0.1 second time step
+  height += velocity * 0.1 # 0.1 second time step
 end
+
+# Determine minimum viable lift-off thrust
+min_thrust = ENGINE_THRUST * MIN_LIFTOFF_THRUST
+
+# Simulate continued liftoff
+while height < 100 # feet
+  if total_mass < MAX_FUEL_MASS
+    fuel_mass += FUEL_FLOW_RATE * 33
+    fuel_mass = [fuel_mass, MAX_FUEL_MASS - DRY_ROCKET_MASS].min
+  end
+  
+  total_mass = DRY_ROCKET_MASS + fuel_mass
+  acceleration = ENGINE_THRUST * 33 / total_mass - GRAVITY
+  velocity += acceleration * 0.1 # 0.1 second time step
+  height += velocity * 0.1 # 0.1 second time step
+end
+
+puts "At 33 engines and #{fuel_mass} kg of fuel, reached #{height.round(2)} meters of lift."
+puts "Minimum viable lift-off thrust: #{min_thrust.round(2)} Newtons."
